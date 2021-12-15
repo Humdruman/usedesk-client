2daacc4bf7b34d5c1181643fc18eed7d1bb20a94
mike <humdruman@gmail.com>
2021-12-16 00:04:52 +0300

    add create comment method

---
+++ lib/modules/tickets/comment.d.ts
@@ -0,0 +1,14 @@
+export declare type CreateCommentRequest = {
+    cc?: string[];
+    bcc?: string[];
+    user_id?: number;
+    type?: 'private|public';
+    files?: string[];
+    from?: 'user' | 'client' | 'trigger';
+    trigger_id?: string;
+};
+export declare type CreateCommentResponse = {
+    status: string;
+    comment_id: number;
+};
+export declare const createComment: (ticketId: number, message: string, params?: CreateCommentRequest) => Promise<CreateCommentResponse>;
--- lib/modules/tickets/index.d.ts
+++ lib/modules/tickets/index.d.ts
@@ -1,4 +1,5 @@
 import { getTickets, TicketsResponse, TicketsRequest } from './tickets';
 import { getTicket, TicketResponse, TicketRequest } from "./ticket";
 import { updateTicket, UpdateTicketResponse, UpdateTicketRequest } from "./updateTicket";
-export { getTickets, TicketsResponse, TicketsRequest, getTicket, TicketResponse, TicketRequest, updateTicket, UpdateTicketRequest, UpdateTicketResponse };
+import { createComment, CreateCommentResponse, CreateCommentRequest } from "./comment";
+export { getTickets, TicketsResponse, TicketsRequest, getTicket, TicketResponse, TicketRequest, updateTicket, UpdateTicketRequest, UpdateTicketResponse, createComment, CreateCommentResponse, CreateCommentRequest };
